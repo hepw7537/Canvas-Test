57088a7af2d75ac793395cb4b5eef86b6f06317f
hepw7537 <32363276+hepw7537@users.noreply.github.com>
2018-12-11 10:49:32 +0000

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,1 +1,15 @@
-console.log("hello");
+let myImage = document.getElementById("myImage");
+let myButtonAdd = document.getElementById("myButtonAdd");
+let myButtonChange = document.getElementById("myButtonChange");
+let myCanvas = document.getElementById("myCanvas");
+let ctx = myCanvas.getContext("2d");
+myButtonChange.addEventListener("click", changeImage,true);
+myButtonAdd.addEventListener("click", addFilter,true);
+
+
+//when page loads make canvas size the same as the image size
+myImage.onload = function() {
+    myCanvas.width = myImage.width;
+   myCanvas.height = myImage.height;
+    ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
+}; 
