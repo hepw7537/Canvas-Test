4102cc24728c68af123bb7ee119178836b9f8cfb
hepw7537 <32363276+hepw7537@users.noreply.github.com>
2018-12-11 11:01:29 +0000

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -29,5 +29,27 @@ myImage.onload = function() {
 
 
 function addFilter(){
+
+  // Read each pixel out of canvas
+  // Array representing, r then g then b values for each pixel
+  let imageData = ctx.getImageData(0, 0, myCanvas.width, myCanvas.height);
+  let dataArray = imageData.data;
+
+  
+  // Loop over each pixel
+  const l = dataArray.length;
+  for(let i = 0; i < l ; i += 4){
+    // dataArray [i] = Red
+    // dataArray [i+1] = Green
+    // dataArray [i+2] = Blue
+    // dataArray [i+3] = Alpha
     
+    // Invert the colours by altering the r, g, b values
+    dataArray[i] = 255 - dataArray[i];
+    dataArray[i + 1] = 255 - dataArray[i + 1];
+    dataArray[i + 2] = 255 - dataArray[i + 2];
+  }
+      // Write the data back to canvas
+  ctx.putImageData(imageData, 0 , 0);
 }
+
